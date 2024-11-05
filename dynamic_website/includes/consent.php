8606ac9c888b71cd1e5420ea223446e052246d24
JerrysBits <topsy0426@yahoo.com>
2024-11-05 12:54:41 +0100

    New Commit

--- dynamic_website/includes/consent.php
+++
@@ -1,9 +0,0 @@
-
-
-
-<div id="consent-popup" class="hidden">
-    <p class="termsCon">
-        By using this site you agree to our <a href="#">Terms and Conditions</a>. 
-        Please <a id="accept" href="#">Accept</a> these before using our site.
-    </p>
-</div>
--- group-two-ecommerce-development/98cf09da-e22c-4bae-9b34-f3f1561a095d/global_variables.js
+++
@@ -1,6 +0,0 @@
-// for load infinity products
-LOAD_INFINITY_CLASSES = {
-  category: ".category",
-  loadMore: ".load-more",
-  appendProducts: ".products.load-infinity",
-};
